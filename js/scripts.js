20709d4c69f72315b79a462ff54fecb603bca22e
Thelongvalleyriver <naomi3@live.nl>
2023-10-28 18:34:54 +0200

    js make hamburger nav work

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,12 @@
 
   console.log("Hi, my name is Naomi and I'm a UI/UX Designer")
+
+
+  function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
